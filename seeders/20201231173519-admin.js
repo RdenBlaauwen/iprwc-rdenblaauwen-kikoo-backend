c90868da7020df383e1aba5d1c876c0b69d19c2c
Robert den Blaauwen <rdenblaauwen@gmail.com>
2021-01-21 18:36:09 +0100

    edit seeders, product seeder now has 10 books

--- seeders/20201231173519-admin.js
+++ seeders/20201231173519-admin.js
@@ -14,6 +14,7 @@ module.exports = {
 						id      : v4() ,
 						password: hash ,
 						username: 'ShopKeeper' ,
+						email   : 'shopkeeper@development.kikoo.nl' ,
 						isAdmin : true
 					}
 				);
--- seeders/20201231181327-test-products.js
+++ seeders/20201231181327-test-products.js
@@ -5,53 +5,79 @@ const{v4} = require('uuid');
 // const tableName = 'product';
 const records = [
 	{
-		name       : 'Dik vet Boek' ,
-		description: 'Lorem ipsum dolor sit amet' ,
-		imageUrl   : 'https://3.bp.blogspot.com/-SR_oJRvj0-k/UQSNhkm80vI/AAAAAAAAAMs/Fv5zVSeZUPk/s1600/5_wikipedia-1.jpg' ,
+		name       : 'Georgische encyclopedie' ,
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, laborum aperiam corporis veritatis assumenda sapiente quia amet nobis iusto eius mollitia libero qui fugit eaque. Tempora in fugit hic eos?' ,
+		imageUrl   : 'https://upload.wikimedia.org/wikipedia/commons/0/09/Encyclopedia_-_Georgia.jpg' ,
 		stock      : 200 ,
-		price      : 20.50
+		price      : 39.98
 	} ,
 	{
-		name       : 'toiletpapier' ,
-		description: 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet' ,
-		imageUrl   : 'https://previews.123rf.com/images/maxxyustas/maxxyustas1502/maxxyustas150200004/36429261-hemorrhoids-concept-toilet-paper-as-grater-3d.jpg' ,
-		stock      : 1500 ,
-		price      : 2.50
-	} ,
-	{
-		name       : 'Argentinie' ,
-		description: 'Na jaren aanmodderen is dit land eindelijk op zoek naar een andere eigenaar!' ,
-		imageUrl   : 'https://www.nationsonline.org/maps/Argentina-political-map.jpg' ,
+		name       : 'Kruidengeneeskundig boek' ,
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, laborum aperiam corporis veritatis assumenda sapiente quia amet nobis iusto eius mollitia libero qui fugit eaque. Tempora in fugit hic eos?' ,
+		imageUrl   : 'https://c.pxhere.com/photos/76/13/antiquariat_old_cookbook_antiquarian_cookbook_book_antique_cookbook_cookbook-452472.jpg!d' ,
 		stock      : 1 ,
-		price      : 0.0097
+		price      : 549.50
 	} ,
 	{
-		name       : 'Een kilo veren' ,
-		description: 'Verpakt in een grote plastic zak' ,
-		imageUrl   : 'https://www.treehugger.com/thmb/c97ot3Wbei4i1fVPuMLNB8se__M=/911x683/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2018__11__feathers-a51ccbea232e481cb263de664914305a.jpg' ,
-		stock      : 20 ,
-		price      : 1.5
+		name       : 'Murder In Old Bombay' ,
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, laborum aperiam corporis veritatis assumenda sapiente quia amet nobis iusto eius mollitia libero qui fugit eaque. Tempora in fugit hic eos?' ,
+		imageUrl   : 'https://fromtheauthors.files.wordpress.com/2020/12/murder-in-old-bombay.png' ,
+		stock      : 150 ,
+		price      : 25
 	} ,
 	{
-		name       : 'Een kilo staal' ,
-		description: 'Een blok van 10x12x24 cm' ,
-		imageUrl   : 'https://static.wikia.nocookie.net/elderscrolls/images/3/3b/Ingotsteel.png/revision/latest?cb=20120410064443' ,
-		stock      : 20 ,
-		price      : 8.95
+		name       : 'Romeo & Juliet' ,
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, laborum aperiam corporis veritatis assumenda sapiente quia amet nobis iusto eius mollitia libero qui fugit eaque. Tempora in fugit hic eos?' ,
+		imageUrl   : 'https://images-na.ssl-images-amazon.com/images/I/81agdswpxAL.jpg' ,
+		stock      : 400 ,
+		price      : 14.99
+	} ,
+	{
+		name       : 'She: A History of Adventure' ,
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, laborum aperiam corporis veritatis assumenda sapiente quia amet nobis iusto eius mollitia libero qui fugit eaque. Tempora in fugit hic eos?' ,
+		imageUrl   : 'https://images-na.ssl-images-amazon.com/images/I/81Ccf2K0axL.jpg' ,
+		stock      : 180 ,
+		price      : 29.99
+	} ,
+	{
+		name       : 'The Lion, the Witch and the Wardrobe' ,
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, laborum aperiam corporis veritatis assumenda sapiente quia amet nobis iusto eius mollitia libero qui fugit eaque. Tempora in fugit hic eos?' ,
+		imageUrl   : 'https://www.memoriapress.com/wp-content/uploads/2012/03/LionWitchWardrobe.jpg' ,
+		stock      : 150 ,
+		price      : 28.50
+	} ,
+	{
+		name       : 'The Hobbit' ,
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, laborum aperiam corporis veritatis assumenda sapiente quia amet nobis iusto eius mollitia libero qui fugit eaque. Tempora in fugit hic eos?' ,
+		imageUrl   : 'https://images-na.ssl-images-amazon.com/images/I/91b0C2YNSrL.jpg' ,
+		stock      : 450 ,
+		price      : 34.99
+	} ,
+	{
+		name       : 'Dream of the Red Chamber' ,
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, laborum aperiam corporis veritatis assumenda sapiente quia amet nobis iusto eius mollitia libero qui fugit eaque. Tempora in fugit hic eos?' ,
+		imageUrl   : 'https://images-na.ssl-images-amazon.com/images/I/81UjsCGC6rL.jpg' ,
+		stock      : 50 ,
+		price      : 11.98
+	} ,
+	{
+		name       : 'The Little Prince' ,
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, laborum aperiam corporis veritatis assumenda sapiente quia amet nobis iusto eius mollitia libero qui fugit eaque. Tempora in fugit hic eos?' ,
+		imageUrl   : 'https://images-na.ssl-images-amazon.com/images/I/81G2R0luQJL.jpg' ,
+		stock      : 350 ,
+		price      : 26.98
+	} ,
+	{
+		name       : 'Harry Potter and the Philosopher\'s Stone' ,
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, laborum aperiam corporis veritatis assumenda sapiente quia amet nobis iusto eius mollitia libero qui fugit eaque. Tempora in fugit hic eos?' ,
+		imageUrl   : 'https://cdn.shopify.com/s/files/1/1416/8662/products/harry_potter_and_the_philosophers_stone_2001_daybill_original_film_art_1200x.jpg?v=1580776459' ,
+		stock      : 550 ,
+		price      : 39.99
 	} ,
 ];
 
 module.exports = {
 	up: async() => {
-		// const instances = records.map( (record) => {
-		// 	record.id = v4();
-		// 	return Product.build(record).get();
-		// });
-		// console.log('seeder products' , instances);
-		// return queryInterface.bulkInsert(
-		// 	tableName ,
-		// 	instances
-		// );
 		const recordsWithIds = records.map( (record) => {
 			record.id = v4();
 			return record;
@@ -60,11 +86,6 @@ module.exports = {
 	} ,
 
 	down: async() => {
-		// return queryInterface.bulkDelete(
-		// 	tableName ,
-		// 	null ,
-		// 	{}
-		// );
 		return Product.destroy();
 	}
 };
